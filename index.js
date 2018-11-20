99f1abdbdaef41361f1902abf1419dfcafa3778f
George Jeffery <georgejeffery@gmail.com>
2018-11-20 11:15:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var i
   for (i=0; i<5; i++){
-    return musicians[i] + "plays" + instruments[i]
+    return musicians[i] + " plays " + instruments[i]
   }
 }
