dcd30e885a6a51dfd9b9d72ed86f4f7b2753ffd6
George Jeffery <georgejeffery@gmail.com>
2018-11-20 11:24:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   var i
   var result = []
- while (i<facts.length()) {
+ while (i<facts.length){
    result[i] = facts [i] + "!!!"
  } 
  return result
